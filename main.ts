052f77c2b71ab3e70a6880808990998a5c7670e5
Ceddifloh <153507235+Ceddifloh@users.noreply.github.com>
2023-12-11 11:36:24 +0100

    Erste Dateien für MakeCode-Projekt

---
+++ main.ts
@@ -0,0 +1,9 @@
+input.onButtonEvent(Button.A, input.buttonEventClick(), function () {
+    music.playMelody("C5 B A G F E D C ", 120)
+})
+basic.forever(function () {
+	
+})
+basic.forever(function () {
+    basic.showIcon(IconNames.Heart)
+})
